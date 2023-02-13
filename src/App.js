deea3983788a814b5abbd672e6e93c56e1fe90d8
newLach <ldpsims@gmail.com>
2023-02-14 01:50:19 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Timer = () =>{
+	const buttons = ["start","up","down","reset","stop"]
+	const[display, setDisplay] = useState(0)
+	const[input, setInput] = useState(0);
+	const[playing, setPlaying] = useState(false)
+
+	
+
+	useEffect(()=>{
+		const intervalId = setInterval(()=>{
+			calculateDisplay();
+		}, 1000)
+		return ()=> {clearInterval(intervalId)};
+	
+			}, )
+
+
+
+
+	const calculateDisplay = () =>{
+			
+
+		if(playing)
+		{
+			setDisplay(display => display + 1)
+		}
+
+		if(display == (input * 12 * 5)){
+			let audio = new Audio("https://cdn.pixabay.com/audio/2022/03/15/audio_9b106fb911.mp3")
+			setPlaying(false);
+			audio.play()
+
+
+
+		}
+
+
+
+	}
+
+
+
+
+	const	handleClick	=	(prop)	=>{
+		switch(prop){
+			case ("start"):
+				setPlaying(true);
+				break;
+			case("stop"):
+				setPlaying(false);
+				break;
+			case("up"):
+				setInput(input =>  input + 5);
+				break;
+			case("down"):
+				setInput(input => input - 5)
+				break;
+			case("reset"):
+				setInput(input => 0)
+				break;
+			default:
+				console.error(prop)
+				break;
+		}
+	}
+
+	return(
+		<div>
+
+		<div>
+		<h1>timer</h1>
+		<ul>
+		{buttons.map((value, index) => (
+			<li key={index} prop={value}>
+			<button 
+			onClick={() => handleClick(value)}>
+			{value}
+			</button>
+			</li>)
+		)}
+		</ul>
+		</div>
+
+
+
+
+		<div>	
+		{input}
+		{"  mins till alarm"}
+		</div>
+
+		<div>
+		{display}
+		{"  seconds passed"}
+		</div>
+
+		</div>
+
+	)
+
 }
-
-export default App;
+export default Timer;
