2521b979eac2c4c36c532dc1838f2040b1c13ef3
newLach <ldpsims@gmail.com>
2023-02-15 01:03:58 +1100

    added react-router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
+import './App.css';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createRoot} from "react-dom/client";
+import {createBrowserRouter, RouterProvider, Route, Link} from 'react-router-dom'
+import {Timer} from './pages/Timer.js'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+const	router	=	createBrowserRouter([
+	{
+		path:'/',
+		element:(<div>"/timer to go to timer app"</div>)
+	},
+	{
+		path:'/timer',
+		element:<Timer/>
+	}
+	
+
+])
+createRoot(document.getElementById("root")).render(
+	<RouterProvider router={router}/>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
