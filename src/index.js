9bd7b6fc73c207ee6d966ba30ceea55b9e8d24ba
newLach <ldpsims@gmail.com>
2023-02-15 17:16:50 +1100

    added navbar

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 import {createRoot} from "react-dom/client";
 import {createBrowserRouter, RouterProvider, Route, Link} from 'react-router-dom'
 import {Timer} from './pages/Timer.js'
-
+import {Home} from './pages/Home.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
 
 const	router	=	createBrowserRouter([
 	{
@@ -13,10 +15,16 @@ const	router	=	createBrowserRouter([
 	{
 		path:'/timer',
 		element:<Timer/>
+	},
+	{
+		path:'/home',
+		element:<Home/>
+
 	}
 	
 
 ])
 createRoot(document.getElementById("root")).render(
-	<RouterProvider router={router}/>
+	<Container><RouterProvider router={router}/>
+	</Container>
 );
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React,{Text} from 'react'
+import {Navigation} from '../components/Navigation.js'
+ 
+export const Home = () =>{
+
+	return(
+		<div>
+		<Navigation/>
+		{"THis is the home screen"}
+		</div>
+	)
+
+}
